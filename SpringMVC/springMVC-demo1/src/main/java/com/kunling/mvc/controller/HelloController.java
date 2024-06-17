f2bdcdef63b7d9edbbd5273e03e754db03bc2307
kunling <1834359580@qq.com>
2024-06-17 18:22:40 +0800

    springmvc tomcat配置

---
+++ SpringMVC/springMVC-demo1/src/main/java/com/kunling/mvc/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.kunling.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+    //  通过@RequestMapping注解，可以通过请求路径匹配要处理的具体的请求
+    //  /表示的当前工程的上下文路径
+    @RequestMapping("/")
+    public String index(){
+        // 返回视图名称
+        return "index";
+    }
+
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
+
+}
